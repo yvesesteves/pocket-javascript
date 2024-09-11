a589c80ac0ed4f5cacb8a18a830fbaf481ed0763
Yves Yan Esteves Pereira <yves.yan@hotmail.com>
2024-09-11 17:43:15 -0300

    projeto rocketseat

---
+++ server/src/db/schema.ts
@@ -0,0 +1,26 @@
+// Criacao das tabelas do BD
+import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2' // criacao de IDS unicos
+
+export const goals = pgTable('goals', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()), // quando inserir um registro, ele preenche o ID automaticamente
+  title: text('title').notNull(),
+  desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
+
+export const goalCompletions = pgTable('goal_completions', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  goalId: text('goal_id')
+    .references(() => goals.id)
+    .notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
